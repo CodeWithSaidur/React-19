0fbc7266d104bd1c8ab4d54c5194dd066a396b33
CodeWithSaidur <sabedbarbhuiya3@gmail.com>
2026-01-31 20:33:28 +0530

    state management 101

--- src/components/05.jsx
+++ src/components/05.jsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
 export default function Five() {
+
+    let [a, seta] = useState(0)
+
+    let clickHendeler = () => {
+        seta(a + 1)
+    }
+
     return (
         <div>
-            
+            <h1>{a}</h1>
+            <button onClick={clickHendeler}>Click</button>
         </div>
     )
 }
