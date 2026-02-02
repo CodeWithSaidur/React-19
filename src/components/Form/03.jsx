bee176d912b924b7b951eb4f966ce94e33331e97
CodeWithSaidur <sabedbarbhuiya3@gmail.com>
2026-02-02 09:39:08 +0530

    refactor form component to include country selection and agreement checkbox

--- src/components/Form/03.jsx
+++ src/components/Form/03.jsx
@@ -1,41 +1,58 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 export default function Page() {
 
-    const [formdata, setFormdata] = useState({
-        gender: ''
-    })
+  const [formdata, setFormdata] = useState({
+    gender: "",
+    country: "india",
+    agree: false
+  })
 
-    let hendelChange = (e) => {
-        setFormdata({ ...formdata, [e.target.name]: e.target.value })
-    }
+  const handelChange = (e) => {
+    const { name, type, value, checked } = e.target;
+    setFormdata((prev) => ({
+      ...prev,
+      [name]: type === "checkbox" ? checked : value
+    }))
+  }
 
-    let hendelSubmit = (e) => {
-        e.preventDefault()
-        console.log(formdata);
-    }
+  const handelSubmit = (e) => {
+    e.preventDefault()
+    console.log(formdata);
+  }
 
-    return (
-        <div>
-            <form>
-                <label>
-                    <input
-                        name='gender'
-                        type="radio"
-                        value='m'
-                        onChange={hendelChange}
-                    /> Mail
-                </label>
-                <label>
-                    <input
-                        name='gender'
-                        type="radio"
-                        value='f'
-                        onChange={hendelChange}
-                    /> Femail
-                </label>
-                <button onClick={hendelSubmit}>Click</button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <form onSubmit={handelSubmit}>
+        <label>
+          <input type="radio" name="gender" value='mail' onChange={handelChange} />
+          Mail
+        </label>
+        <label>
+          <input type="radio" name="gender" value='femail' onChange={handelChange} />
+          Femail
+        </label>
+
+        <br />
+
+        <label htmlFor="">
+          <select name="country" id="" onChange={handelChange} value={formdata.country}>
+            <option value="india">India</option>
+            <option value="usa">USA</option>
+            <option value="uk">UK</option>
+            <option value="canada">Canada</option>
+          </select>
+        </label>
+
+        <br />
+
+        <label htmlFor="">
+          <input type="checkbox" name="agree" onChange={handelChange} checked={formdata.agree} />
+          I agree terms and conditions
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }
