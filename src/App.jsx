263e4f65af079275221bff4d85b55e69c94d55cf
CodeWithSaidur <sabedbarbhuiya3@gmail.com>
2026-03-04 21:22:13 +0530

    use call back

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
-export default function App() {
-  return (
-    <div>App</div>
-  )
-}
+// import { useState } from 'react'
 
+// export default function App() {
+
+//   const [c, setc] = useState(0)
+
+//   let inc = () => {
+//     setc(c + 1)
+//     console.log('----------'); 
+//   }
+
+//   return (
+//     <div>
+//       <p>{c}</p>
+//       <button onClick={inc}>+</button>
+//     </div>
+//   )
+// }
+
+
+import { useCallback, useState } from "react";
+
+export default function App() {
+  const [c, setC] = useState(0);
+
+  const inc = useCallback(() => {
+    setC((prev) => prev + 1);
+    console.log("-----------------");
+  }, []);
+
+  return (
+    <div>
+      <p>{c}</p>
+      <button onClick={inc}>+</button>
+    </div>
+  );
+}
